d1d3242b9cc1ce0933c3344d405973a61780bd78
rbtstha <robitstha90@gmail.com>
2016-09-08 17:46:34 +0545

    Final edit
    3rd day final commmit

---
+++ style.cs
@@ -0,0 +1,60 @@
+body {
+  background-color: #eee;
+  color: purple;
+  margin: 0px;
+  padding: 0px; }
+
+input {
+  font-family: football; }
+
+h1 {
+  display: block;
+  background: #8e44ad;
+  color: #eee;
+  padding: 10px;
+  width: 100%;
+  height: 80px;
+  margin: 0px;
+  font-family: monospace;
+  font-size: 55px;
+  margin-bottom: 50px; }
+
+table {
+  font-family: sans-serif;
+  margin: 0px auto;
+  width: 90%;
+  border-collapse: collapse; }
+  table th, table td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd; }
+  table th {
+    background: purple;
+    color: white; }
+  table tr:hover {
+    background-color: #FF9EFF; }
+  table span.emoji {
+    color: #000;
+    font-size: 2em;
+    transition: all 0.3s;
+    cursor: pointer; }
+    table span.emoji:hover {
+      font-size: 4em; }
+
+.btn-download {
+  color: purple;
+  padding: 10px 20px 10px 20px;
+  border: 2px solid purple;
+  background: transparent;
+  border-radius: 3px;
+  transition: all 0.3s;
+  margin: 10px; }
+  .btn-download:hover {
+    background: #FF8000;
+    color: black; }
+
+img {
+  border-radius: 0px;
+  margin-right: 50px; }
+
+/*# sourceMappingURL=style.cs.map */
